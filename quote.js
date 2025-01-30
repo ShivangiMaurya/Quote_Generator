be1fcf21bbc9f1e428927909579fec243f2342c2
ShivangiMaurya <shivangimouryaceo@gmail.com>
2025-01-30 20:54:21 +0530

    quote

--- quote.js
+++ quote.js
@@ -4,7 +4,13 @@ const quotes = [
   "Life is what happens when you're busy making other plans. – John Lennon",
   "Don't watch the clock; do what it does. Keep going. – Sam Levenson",
   "Success is not how high you have climbed, but how you make a positive difference to the world. – Roy T. Bennett",
-  "Happiness is not by chance, but by choice. – Jim Rohn"
+  "Happiness is not by chance, but by choice. – Jim Rohn",
+
+  "A name is the blueprint of the thing we call character. You ask, what's in a name? I answer, just about everything you do. – Morris Mandel",
+  "Names are the sweetest and most important sound in any language. – Dale Carnegie",
+  "Words have meaning and names have power. – Anonymous",
+  "A name represents identity, a deep feeling, and holds tremendous significance to its owner. – Rachel Ingber",
+  "Give a thing a name and it will somehow come to be. – George R.R. Martin"
 ];
 
 const quoteElement = document.getElementById('quote');
