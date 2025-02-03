c0c516a996434c2134ad9777ad052e923a5640ab
ShivangiMaurya <shivangimouryaceo@gmail.com>
2025-02-03 22:48:54 +0530

    quotes

--- quote.js
+++ quote.js
@@ -10,6 +10,18 @@ const quotes = [
   "Names are the sweetest and most important sound in any language. – Dale Carnegie",
   "Words have meaning and names have power. – Anonymous",
   "A name represents identity, a deep feeling, and holds tremendous significance to its owner. – Rachel Ingber",
+  "Give a thing a name and it will somehow come to be. – George R.R. Martin",
+
+  "A name is the blueprint of the thing we call character. You ask, what's in a name? I answer, just about everything you do. – Morris Mandel",
+  "Names are the sweetest and most important sound in any language. – Dale Carnegie",
+  "Words have meaning and names have power. – Anonymous",
+  "A name represents identity, a deep feeling, and holds tremendous significance to its owner. – Rachel Ingber",
+  "Give a thing a name and it will somehow come to be. – George R.R. Martin",
+
+  "A name is the blueprint of the thing we call character. You ask, what's in a name? I answer, just about everything you do. – Morris Mandel",
+  "Names are the sweetest and most important sound in any language. – Dale Carnegie",
+  "Words have meaning and names have power. – Anonymous",
+  "A name represents identity, a deep feeling, and holds tremendous significance to its owner. – Rachel Ingber",
   "Give a thing a name and it will somehow come to be. – George R.R. Martin"
 ];
 
